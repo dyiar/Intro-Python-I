d6fc8974f8a0e4bdc4b076a0325e5e726a5a3f46
dyiar <austin.hl.blake@gmail.com>
2019-03-26 17:03:38 -0600

    end of assignment

--- src/slices.py
+++ src/slices.py
@@ -10,12 +10,16 @@ Use Python's slice syntax to achieve the following:
 """
 
 a = [2, 4, 1, 7, 9, 6]
+    #0, 1, 2, 3, 4, 5
+    #-5, -4, -3, -2, -1
 
 # Output the second element: 4:
-print(a[1:2])
+# print(a[1:2])
+print(a[1])
 
 # Output the second-to-last element: 9
-print(a[4:5])
+# print(a[4:5])
+print(a[-2])
 
 # Output the last three elements in the array: [7, 9, 6]
 print(a[3:])
